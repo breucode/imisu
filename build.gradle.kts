581541421f4080c56e09770edacb4732f8116c9a
Pascal Breuer <pbreuer@breuco.de>
2020-03-23 21:45:55 +0100

    Upgraded Kotlin to 1.3.71

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.70"
+    kotlin("jvm") version "1.3.71"
     id("application")
     id("com.github.johnrengelman.shadow") version "5.2.0"
 
