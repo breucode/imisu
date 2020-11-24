d45f7bb3db551b838cdd57f7281c19fac60efd7b
breucode <pbreuer@breuco.de>
2020-11-24 20:32:16 +0100

    Add line breaks at the end of files and remove graal native image script

--- build-native-image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-native-image --no-fallback --allow-incomplete-classpath --initialize-at-build-time=org.minidns,org.slf4j -jar build/libs/imisu-0.0.1-all.jar build/libs/imisu-0.0.1-all
