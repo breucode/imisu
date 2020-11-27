977aa2d6933f304c3449482d8992518940118a2d
breucode <pbreuer@breuco.de>
2020-11-27 15:40:23 +0100

    Build native image via gradle and format json

--- build-native-image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-native-image --no-fallback --allow-incomplete-classpath --initialize-at-build-time=org.slf4j.impl.SimpleLogger,org.slf4j.LoggerFactory,org.slf4j.impl.StaticLoggerBinder,org.minidns -jar build/libs/imisu.jar build/libs/imisu
