895b9f34512cf68acf2c9211f0a061ecae4cf2a4
breucode <pbreuer@breuco.de>
2021-09-15 23:21:54 +0200

    Migrated ApplicationTest to mockito

--- src/test/kotlin/de/breuco/imisu/ApplicationTest.kt
+++ src/test/kotlin/de/breuco/imisu/ApplicationTest.kt
@@ -2,69 +2,43 @@ package de.breuco.imisu
 
 import de.breuco.imisu.api.Api
 import de.breuco.imisu.config.ApplicationConfig
-import io.mockk.clearAllMocks
-import io.mockk.confirmVerified
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.mockkStatic
-import io.mockk.verify
 import mu.KLogger
-import org.http4k.server.asServer
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.koin.core.context.stopKoin
+import org.mockito.Answers
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.reset
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 class ApplicationTest {
 
-  private val apiMock = mockk<Api>()
-  private val appConfigMock = mockk<ApplicationConfig>()
-  private val loggerMock = mockk<KLogger>(relaxed = true)
+  private val appConfigMock = mock<ApplicationConfig>(defaultAnswer = Answers.RETURNS_DEEP_STUBS)
+  private val loggerMock = mock<KLogger>(lenient = true)
 
   private lateinit var underTest: Application
 
   @BeforeEach
   fun beforeEach() {
-    underTest = Application(apiMock, appConfigMock, loggerMock)
+    underTest = Application(Api(appConfigMock, mock()), appConfigMock, loggerMock)
   }
 
   @AfterEach
   fun afterEach() {
     stopKoin()
-    confirmVerified(apiMock)
-    clearAllMocks()
-  }
-
-  @Test
-  fun `normal start`() {
-    every { appConfigMock.userConfig.serverPort } returns 8080
-    every { appConfigMock.userConfig.exposeFullApi } returns false
-
-    mockkStatic("org.http4k.server.Http4kServerKt")
-
-    every { apiMock.routing().asServer(any()).start() } returns mockk()
-
-    underTest.run()
-
-    verify(exactly = 1) {
-      apiMock.routing().asServer(any()).start()
-    }
+    reset(appConfigMock, loggerMock)
   }
 
   @Test
   fun `log warning when full api is exposed`() {
-    every { appConfigMock.userConfig.serverPort } returns 8080
-    every { appConfigMock.userConfig.exposeFullApi } returns true
-
-    mockkStatic("org.http4k.server.Http4kServerKt")
-
-    every { apiMock.routing().asServer(any()).start() } returns mockk()
+    whenever(appConfigMock.userConfig.serverPort).thenReturn(8080)
+    whenever(appConfigMock.userConfig.exposeFullApi).thenReturn(true)
 
     underTest.run()
 
-    verify(exactly = 1) {
-      apiMock.routing().asServer(any()).start()
-      loggerMock.warn(any<() -> Any?>())
-    }
+    verify(loggerMock).warn(any<() -> Any?>())
   }
 }
